6941d85033b266919b8594e28bcce86be117b339
mudarisovmur <mudarisovmur@gmail.com>
2024-01-06 14:22:42 +0700

    add requirements

--- FastLab.py
+++ FastLab.py
@@ -8,6 +8,4 @@ def sum_two_args(x,y):
 # Hello World route
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+ return {"Hello": "World"}
